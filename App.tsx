a18fe3a4286c9371bf4c293c04317c093f9ba990
Cristian Rojas <cristian@v-labs.fr>
2022-02-14 12:19:11 +0100

    Fix styled components not working

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Text>Test</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Test = styled.Text``;
