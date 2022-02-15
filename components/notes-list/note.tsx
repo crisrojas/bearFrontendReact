58f3be728df538b647243483a7ce892eb404a9f3
Cristian Rojas <cristian@v-labs.fr>
2022-02-15 15:15:56 +0100

    Move state logic from Note component to parent component NoteList

--- components/notes-list/note.tsx
+++ components/notes-list/note.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Text, View  } from 'react-native';
+import { View, TouchableWithoutFeedback  } from 'react-native';
 import styled from "styled-components/native";
 import Colors from "../../UI/colors"
 import { NativeBaseProvider, Box, HStack, VStack} from "native-base";
@@ -8,62 +8,61 @@ import PinnedIcon from '../../assets/pinned.svg.tsx'
 interface NoteModel {
 	title: String
 	summary: String
-	lastTime: String
+	modifiedSince: String
 	pinned: Bool
 }
 
 const Note = (props) => {
-	
-	const didSelectNote = () => {
-		console.log('You selected the note')
-	}
-	
-	const [active, setActive] = useState(false)
-	
-	useEffect(() => {
-		didSelectNote()
-	}, [active])
-	
-		return (
-				<NoteWrapper 
-				onClick={() => setActive(!active)}
-				active={active}
-				>
+
+	return (
+		<TouchableWithoutFeedback onPress={props.onPress}>
+			<NoteWrapper
+			isSelected={props.isSelected}>
+			
+				  <HStack space={3} justifyContent="left">
+					<VStack space={3}>
+						<Text>{props.modifiedSince}</Text>
+						
+						<Text><PinnedIcon/></Text>
+					</VStack>
 					
-					  <HStack space={3} justifyContent="left">
-						<VStack space={3}>
-							<Text>{props.lastTime}</Text>
-							<Text><PinnedIcon/></Text>
-							
-							
-						</VStack>
-						
-						<VStack space={2}>
-							  <Text>{props.title}</Text>
-							  <Text>{props.summary}</Text>
-							  <Separator></Separator>
-						  </VStack>
-						</HStack>
-						
-			</NoteWrapper>
-		)
-	
+					<VStack space={2}>
+						  <Title>{props.title}</Title>
+						  <Text>{props.summary}</Text>
+						  <Separator></Separator>
+					  </VStack>
+			
+					</HStack>
+					
+		</NoteWrapper>
+		</TouchableWithoutFeedback>
+	)
 }
 
 export default Note
 
+const Title = styled.Text`
+	font-family: 'AvenirNextLTPro-Bold'
+	color: ${Colors.titleTextColor}
+`
+
+const Text = styled.Text`
+	color: ${Colors.textBaseColor}
+`
+
 const NoteWrapper = styled.View`
 	border-style: solid; 
-	border-left-width: 5px;
-	border-left-color:  ${props => props.active ?  Colors.noteCellSelectedBorderColor : Colors.background};
-	backgroundColor: ${props => props.active ? Colors.noteCellSelectedBgColor :  Colors.background};
+	border-left-width: 6px;
+	border-left-color:  ${props => props.isSelected ?  Colors.noteCellSelectedBorderColor : Colors.background};
+	backgroundColor: ${props => props.isSelected ? Colors.noteCellSelectedBgColor :  Colors.background};
 	padding-left: 20px;
 	padding-top: 12px;
+	transition: 0.5s;
 `;
 
 const Separator = styled.View`
 	height: 1px;
 	width: 100%;
-	background: black;
+	background: ${Colors.separatorColor};
 	margin-top: 12px;
 `;
--- components/notes-list/notes-list.tsx
+++ components/notes-list/notes-list.tsx
@@ -1,20 +1,102 @@
-import React from "react";
-import { SafeAreaView, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, Text, ScrollView, TouchableOpacity } from 'react-native';
 import styled from "styled-components";
 import Note from './note'
 
 const NotesList = () => {
+	
+	const [selectedId, setSelectedId] = useState("0")
+	
+
+	const didSelectNote = () => {
+		/* @todo: Fetch & show note content from id */
+	}
+	
+	useEffect(() => {
+		didSelectNote()
+	}, [selectedId])
+	
+	
   return (
-	  <View>
+	  <ScrollView>
 	  <Text>Hi, I'm a notes list</Text>
- 	 	<Note 
-		  lastTime="3S" 
-		  title="Note title" 
-		  summary="Note summary"
-		  pinned="true"
-		/>
-	  </View>
+	 
+	  {items.map((item, index) => (
+		  
+			 <Note 
+			 	 onPress={() => setSelectedId(item.id)}
+				 modifiedSince={item.modifiedSince} 
+				 title={item.title}
+				 summary={item.summary}
+				 pinned={item.pinned}
+				 isSelected={item.id == selectedId}
+				 
+			 />
+		
+		))}
+		
+ 	 	
+	  </ScrollView>
   )
 }
 
 export default NotesList;
+
+const items = [
+	{
+		id: "1",
+		modifiedSince: "3S",
+		title: "Note title",
+		summary: "Note summary",
+		pinned: "true",
+	},
+	{
+		id: "2",
+		modifiedSince: "3S",
+		title: "Note title",
+		summary: "Note summary",
+		pinned: "true"
+	},
+	{
+		id: "3",
+		modifiedSince: "3S",
+		title: "Note title",
+		summary: "Note summary",
+		pinned: "false",
+	},
+	{
+		id: "4",
+  		modifiedSince: "3S",
+  		title: "Note title",
+  		summary: "Note summary",
+  		pinned: "false"
+	},
+	{
+		id: "5",
+		  modifiedSince: "3S",
+		  title: "Note title",
+		  summary: "Note summary",
+		  pinned: "false"
+	},
+	{
+		id: "6",
+		  modifiedSince: "3S",
+		  title: "Note title",
+		  summary: "Note summary",
+		  pinned: "false"
+	},
+	{
+		id: "7",
+		  modifiedSince: "3S",
+		  title: "Note title",
+		  summary: "Note summary",
+		  pinned: "false"
+	},
+	{
+		id: "8",
+		  modifiedSince: "3S",
+		  title: "Note title",
+		  summary: "Note summary",
+		  pinned: "false"
+	},
+];
